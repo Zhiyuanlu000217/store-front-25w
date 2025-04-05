373a6a91f2e1ed12ea9e94226536988b2fd9facf
Zee.L <53048355+ZeeL981210@users.noreply.github.com>
2025-04-05 03:42:20 -0400

    Update image config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
+  images: {
+    domains: ['oaidalleapiprodscus.blob.core.windows.net'],
+  },
 };
 
 module.exports = nextConfig; 
